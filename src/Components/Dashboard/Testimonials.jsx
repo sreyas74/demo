9ee3432a1c0cc35eb2a4a4938450c44a5c56176b
sreyas p v <110050231+sreyas74@users.noreply.github.com>
2024-11-09 13:12:58 +0530

    initial commit

--- src/Components/Dashboard/Testimonials.jsx
+++ src/Components/Dashboard/Testimonials.jsx
@@ -1,123 +1,68 @@
-import React, { useState } from "react";
-import styles from "./Testimonials.module.css";
-import Carousel from "react-bootstrap/Carousel";
-function Testimonials() {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
+import React from "react";
+import styles from "./SubscriptionBenefits.module.css";
+import { Button } from "@material-ui/core";
+function SubscriptionBenefits() {
   return (
-    <div className={styles.Testimonials}>
-      <div className={styles.Partners}>
-        <div className={styles.Partners__Images}>
+    <div className={styles.Subscriptions}>
+      <h4>Subscription Benefits</h4>
+      <div className={styles.Subscriptions__Icons}>
+        <div>
           <img
-            src="https://www.revv.co.in/imgs/company_logo/the-hindu-newpaper.webp"
-            alt="The Hindu"
+            src="https://www.revv.co.in/grapheneImages/newopen/zero.svg"
+            alt="zero"
           />
+          <h5>No Down Payment</h5>
         </div>
-        <div className={styles.Partners__Images}>
+        <div>
           <img
-            src="https://www.revv.co.in/imgs/company_logo/The-Economic-Times.webp"
-            alt="The Economic Times"
+            src="https://www.revv.co.in/grapheneImages/newopen/cheaper.svg"
+            alt="zero"
           />
+          <h5>Cheaper than EMI</h5>
         </div>
-        <div className={styles.Partners__Images}>
+        <div>
           <img
-            src="https://www.revv.co.in/imgs/company_logo/Indian_express.webp"
-            alt="The Indian Express"
+            src="https://www.revv.co.in/grapheneImages/newopen/maintainance.svg"
+            alt="zero"
           />
+          <h5>Free Service & maintainance</h5>
         </div>
-        <div className={styles.Partners__Images}>
+        <div>
           <img
-            src="https://www.revv.co.in/imgs/company_logo/BW-logo.png"
-            alt="Business World"
+            src="https://www.revv.co.in/grapheneImages/newopen/return.svg"
+            alt="zero"
           />
-        </div>
-        <div className={styles.Partners__Images}>
-          <img
-            src="https://www.revv.co.in/imgs/company_logo/YourStory.webp"
-            alt="YourStory"
-          />
-        </div>
-        <div className={styles.Partners__Images}>
-          <img
-            src="https://www.revv.co.in/imgs/company_logo/businessinsider.png"
-            alt="businessinsider"
-          />
+          <h5>Extend or return anytime</h5>
         </div>
       </div>
-      <div className={styles.Journey}>
-        <h4>Our Journey so far</h4>
-        <div className={styles.Journey__Icons__Container}>
-          <div className={styles.Journey__Icons}>
-            <img
-              src="https://www.revv.co.in/grapheneImages/newopen/ic-happy-cus.svg"
-              alt="revvers"
-            />
-            <div>
-              <h4>1Mn+</h4>
-              <p>Happy Revvers</p>
-            </div>
-          </div>
-          <div className={styles.Journey__Icons}>
-            <img
-              src="https://www.revv.co.in/grapheneImages/newopen/ic-across-india.svg"
-              alt="location"
-            />
-            <div>
-              <h4>22+ Cities</h4>
-              <p>Accross India</p>
-            </div>
-          </div>
-          <div className={styles.Journey__Icons}>
-            <img
-              src="https://www.revv.co.in/grapheneImages/newopen/ic-subscription.svg"
-              alt="kms travelled"
-            />
-            <div>
-              <h4>50Mn+</h4>
-              <p>Kns Travelled</p>
-            </div>
-          </div>
-          <div className={styles.Journey__Icons}>
-            <img
-              src="https://www.revv.co.in/grapheneImages/newopen/ic-customer-rating.svg"
-              alt="rating"
-            />
-            <div>
-              <h4>4.8/5</h4>
-              <p>20k+ reviewers</p>
-            </div>
-          </div>
+      <div className={styles.Subscriptions__Information}>
+        <div className={styles.Subscriptions__Information__Left}>
+          <h4>What is Revv Subscription? </h4>
+          <p>
+            It’s a different way of owning a car, without any down payment or
+            car loan. Your monthly fee covers insurance, service & maintenance.
+            Plus, there are no long term commitments - you can return, extend or
+            buy-out the car when you want. And all this at a price cheaper than
+            an EMI!
+          </p>
+          <Button className={styles.Subscriptions__Information__Button}>
+            Know More
+          </Button>
         </div>
-      </div>
-
-      <div className={styles.CustomerReviews}>
-        <h4>What our Customers are saying</h4>
-        <Carousel activeIndex={index} onSelect={handleSelect} interval={500000} indicators={false}>
-          <Carousel.Item>
-            <h5>SURYA GVS</h5>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
-          </Carousel.Item>
-        </Carousel>
+        <div className={styles.Subscriptions__Information__Right}>
+          <iframe
+            title="video"
+            width="515"
+            height="266"
+            src="https://www.youtube.com/embed/1YvzeCxiJn0"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Testimonials;
+export default SubscriptionBenefits;
