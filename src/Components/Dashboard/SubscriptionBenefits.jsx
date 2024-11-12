2d4029f129641b5eb96941f332d36f0553e0c701
sreyas p v <110050231+sreyas74@users.noreply.github.com>
2024-11-12 14:43:41 +0530

    box gap

--- src/Components/Dashboard/SubscriptionBenefits.jsx
+++ src/Components/Dashboard/SubscriptionBenefits.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import styles from './SubscriptionBenefits.module.css';
 
 const cars = [
-    { name: 'Wagon R', imageUrl: 'https://github.com/sreyas74/demo/blob/master/Japan_s_Ten_Most_Lovable_Pocket-Sized_Kei_Cars-removebg-preview.png?raw=true', rate: '₹1500/day' },
-    { name: 'BMW i8', imageUrl: '/images/bmw.jpg', rate: '₹150/day' },
-    { name: 'Audi A8', imageUrl: '/images/audi.jpg', rate: '₹130/day' },
-    { name: 'Mercedes C-Class', imageUrl: '/images/mercedes.jpg', rate: '₹120/day' },
+    { name: 'Vespa', imageUrl: 'https://github.com/sreyas74/demo/blob/master/Japan_s_Ten_Most_Lovable_Pocket-Sized_Kei_Cars-removebg-preview.png?raw=true', rate: '₹1500/day' },
+    { name: 'Fascino', imageUrl: '/images/bmw.jpg', rate: '₹150/day' },
+    { name: 'Himalayan', imageUrl: '/images/audi.jpg', rate: '₹130/day' },
+    { name: 'Bullet classic', imageUrl: '/images/mercedes.jpg', rate: '₹120/day' },
 ];
 
 const SubscriptionBenefits = () => {
