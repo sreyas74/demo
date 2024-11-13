b1872c1d17e793a5fe4901c106373afc9c6adfd1
abhinav <abhinavsreekumar2002@gmail.com>
2024-11-13 13:33:42 +0530

    imageurl to image files

--- src/Components/Dashboard/SubscriptionBenefits.jsx
+++ src/Components/Dashboard/SubscriptionBenefits.jsx
@@ -1,11 +1,13 @@
 // SubscriptionBenefits.jsx
 import React from 'react';
 import styles from './SubscriptionBenefits.module.css';
+import ertiga from './images/ertiga.png';
+
 
 const cars = [
     { name: 'Vespa', imageUrl: 'https://github.com/sreyas74/demo/blob/master/Japan_s_Ten_Most_Lovable_Pocket-Sized_Kei_Cars-removebg-preview.png?raw=true', rate: '₹1500/day' },
-    { name: 'Fascino', imageUrl: '/images/bmw.jpg', rate: '₹150/day' },
-    { name: 'Himalayan', imageUrl: '/images/audi.jpg', rate: '₹130/day' },
+    { name: 'Fascino', imageUrl: ertiga, rate: '₹150/day' },
+    { name: 'Himalayan', imageUrl: '../images/wagonr.png', rate: '₹130/day' },
     { name: 'Bullet classic', imageUrl: '/images/mercedes.jpg', rate: '₹120/day' },
 ];
 
