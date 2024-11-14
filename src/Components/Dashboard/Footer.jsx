d7dcbe51df2c183093cd8453d1a2f421cb4bbbbb
sreyas p v <110050231+sreyas74@users.noreply.github.com>
2024-11-14 20:42:25 +0530

    change to img

--- src/Components/Dashboard/Footer.jsx
+++ src/Components/Dashboard/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import logo from "../Dashboard/images/logo2.svg";
+import fb from "../Dashboard/images/fb.svg";
+import instagram from "../Dashboard/images/instagram.svg"
+import x from  "../Dashboard/images/x.svg"
+import youtube from "../Dashboard/images/youtube.svg"
+import linkedin from "../Dashboard/images/linkedin.svg"
 
 // Remove useHistory import since it's not being used
 // import { useHistory } from 'react-router-dom';
@@ -67,36 +73,36 @@ function Footer() {
       <div className={styles.Footer__Bottom}>
         <div className={styles.Footer__Bottom__Column__One}>
           <div className={styles.Footer__Bottom__Column__Top}>
-            <img src="https://raw.githubusercontent.com/sreyas74/trysample-/68ba0ecbb2e84573d79bca868624b3a6f41c9662/og%20logo%20(14).svg" alt="logo" />
+            <img src={logo} alt="logo" />
           </div>
           <div className={styles.Footer__Bottom__Column__Mid}>
             <button onClick={handleFacebookClick}>
               <img
-                src="https://raw.githubusercontent.com/sreyas74/trysample-/e88de68fb4786086033c8dff46b96efd546a4b41/fb%2029.svg"
+                src={fb}
                 alt="facebook"
               />
             </button>
             <button onClick={handleInstagramClick}>
               <img
-                src="https://raw.githubusercontent.com/sreyas74/trysample-/b319b14a208a3c0857c92823da4eb2a5ec1ec812/instu%20(1).svg"
+                src={instagram}
                 alt="instagram"
               />
             </button>
             <button onClick={handleXClick}>
               <img
-                src="https://raw.githubusercontent.com/sreyas74/trysample-/86176a16775447d9c0593e4c7ed31fdab4269928/twituu%20(1).svg"
+                src={x}
                 alt="x"
               />
             </button>
             <button onClick={handleYoutubeClick}>
               <img
-                src="https://raw.githubusercontent.com/sreyas74/trysample-/100512e81f82d1d8223b7c01379255fd2abb9f26/yyyu1%20(1).svg"
+                src={youtube}
                 alt="youtube"
               />
             </button>
             <button onClick={handlelinkedinClick}>
               <img
-                src="https://raw.githubusercontent.com/sreyas74/trysample-/273c70061fd0ff18ecaa88d46615e71d33ff6c04/link%20(1).svg"
+                src={linkedin}
                 alt="linkedin"
               />
             </button>
